0a51ca750b7bf4cf6d466391be4d117f2546fdb3
sinatraclawbot <sinatraclawbot@gmail.com>
2026-03-27 20:09:50 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,18 +4,24 @@ import os
 TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN)
 
+
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Привет. Нажмите 'Создать заявку'")
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn = telebot.types.KeyboardButton("Создать заявку")
+    markup.add(btn)
+    bot.send_message(message.chat.id, "Привет. Нажмите 'Создать заявку'", reply_markup=markup)
 
-@bot.message_handler(func=lambda message: message.text == "Создать заявку")
-def order(message):
-    bot.send_message(message.chat.id, "Введите вид услуги:")
 
-bot.polling()
 @bot.message_handler(commands=['id'])
 def get_id(message):
-    bot.send_message(message.chat.id, f"Ваш ID: {message.chat.id}")
-    @bot.message_handler(commands=['id'])
-def get_id(message):
     bot.send_message(message.chat.id, f"Ваш Telegram ID: {message.chat.id}")
+
+
+@bot.message_handler(func=lambda message: message.text == "Создать заявку")
+def create_order(message):
+    bot.send_message(message.chat.id, "Введите вид услуги:")
+
+
+print("Bot started...")
+bot.polling()
